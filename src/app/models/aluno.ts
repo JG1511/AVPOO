f6631d61e9798c902626da0e7d8da30769fbc588
JG1511 <joaotsi2024@gmail.com>
2025-02-10 10:10:34 -0300

    implementação de mais atributos para as classes

--- src/app/models/aluno.ts
+++ src/app/models/aluno.ts
@@ -1,12 +1,15 @@
 import { Pessoa } from "./pessoa";
+import { Professor } from "./professor";
 
 export class Aluno extends Pessoa {
     curso : string;
     notas : number
+    professor : Professor
 
-    constructor(nome : string, cpf : string, data_nascimento : Date, endereco : string , curso : string, notas : number, ){
+    constructor(nome : string, cpf : string, data_nascimento : Date, endereco : string , curso : string, notas : number, professor : Professor ){
         super(nome,cpf,data_nascimento,endereco);
         this.curso = curso;
         this.notas = notas
+        this.professor = professor
     }
 }
--- src/app/models/professor.ts
+++ src/app/models/professor.ts
@@ -1,2 +1,14 @@
-export class Professor {
+import { Pessoa } from "./pessoa";
+
+export class Professor extends Pessoa{
+    salario : number;
+    especializacao : string;
+    disciplinas_ministradas : string;
+
+    constructor(nome :string, cpf : string, data_nascimento : Date, endereco : string, salario : number, especializacao : string, disciplinas_ministradas : string){
+        super(nome,cpf,data_nascimento,endereco)
+        this.salario = salario;
+        this.especializacao = especializacao;
+        this.disciplinas_ministradas = disciplinas_ministradas;
+    }
 }
