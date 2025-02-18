e81ad7da521f5f51e65e85a5fe97026e71c64b47
JG1511 <joaotsi2024@gmail.com>
2025-02-18 10:59:05 -0300

    criação dos forms retivos do professor e do aluno

--- src/app/pages/forms-login/forms-login.component.ts
+++ src/app/pages/forms-login/forms-login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CadastroService } from '../../services/cadastro.service';
+import { Professor } from '../../models/professor';
 
 @Component({
   selector: 'app-forms-login',
@@ -9,13 +10,28 @@ import { CadastroService } from '../../services/cadastro.service';
   styleUrl: './forms-login.component.css'
 })
 export class FormsLoginComponent {
-  userForm : FormGroup;
+  alunoForm : FormGroup;
+  professorForm : FormGroup;
   
   constructor(private cadastroService : CadastroService){
-    this.userForm = new FormGroup({
-      name : new FormControl('',Validators.required),
-      
-
+    this.alunoForm = new FormGroup({
+      nome_do_aluno : new FormControl('',Validators.required),
+      cpf_do_aluno : new FormControl('',Validators.required),
+      data_nascimento_do_aluno : new FormControl('', Validators.required),
+      endereco_do_aluno : new FormControl('',Validators.required),
+      curso_do_aluno : new FormControl('',Validators.required),
+      nota_do_aluno : new FormControl('',Validators.required),
+    }),
+    this.professorForm = new FormGroup({
+      nome_do_professor : new FormControl('',Validators.required),
+      cpf_do_professor : new FormControl('',Validators.required),
+      data_nascimento_do_professor : new FormControl('', Validators.required),
+      endereco_do_professor : new FormControl('',Validators.required),
+      salario_do_profesor : new FormControl('',Validators.required),
+      especializacao_do_professor : new FormControl('',Validators.required),
+      disciplinas_ministradas_do_professor : new FormControl('',Validators.required)
     })
   }
+
+  
 }
