542339917b44567a507285ab9b6211213f756c03
JG1511 <joaotsi2024@gmail.com>
2025-02-18 22:02:10 -0300

    configuração das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FormsLoginComponent } from './pages/forms-login/forms-login.component';
+import { ListaComponent } from './pages/lista/lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'cadastro' ,
+        title: 'Cadastro' ,
+        component : FormsLoginComponent
+    },
+    {
+        path: 'home-lista',
+        title: 'Home',
+        component : ListaComponent
+    },
+
+    {path : '', redirectTo:'/cadastro', pathMatch : 'full'}
+
+];
